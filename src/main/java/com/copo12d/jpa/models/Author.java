c9465c13e75053f301fad23bba57708dcbc40454
Angel <acalmenc@gmail.com>
2025-03-28 20:05:58 -0400

    models for data base

--- src/main/java/com/copo12d/jpa/models/Author.java
+++ src/main/java/com/copo12d/jpa/models/Author.java
@@ -1,19 +1,46 @@
 package com.copo12d.jpa.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Entity
+@Table(name = "AUTHOR_TBL")
 @Data
 @NoArgsConstructor
 public class Author {
     @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "author_seq"
+    )
+    @SequenceGenerator(
+            name = "author_seq",
+            sequenceName = "author_seq",
+            allocationSize = 1
+    )
     private Integer id;
+    @Column(
+            name ="f_name"
+    )
     private String firstName;
     private String lastName;
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
-    private Integer age;
+    private int age;
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private LocalDate createAt;
+    @Column(
+            insertable = false
+    )
+    private LocalDate lastUpdateAt;
 
 }
