267139870c7a34b2a7564120aa40a22cd09130c2
Angel <acalmenc@gmail.com>
2025-03-29 23:58:09 -0400

    CommandLineRunner function added

--- src/main/java/com/copo12d/jpa/JpaApplication.java
+++ src/main/java/com/copo12d/jpa/JpaApplication.java
@@ -1,13 +1,30 @@
 package com.copo12d.jpa;
 
+import com.copo12d.jpa.models.Author;
+import com.copo12d.jpa.repositories.AuthorRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AuthorRepository authorRepository) {
+		return args -> {
+			var author = Author.builder()
+					.firstName("Angel")
+					.lastName("Calmen")
+					.age(20)
+					.email("acalmenc@gmail.com")
+					.build();
+			authorRepository.save(author);
+		};
 	}
 
 }
